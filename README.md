c28cc279de0614f3a4e466952237df5f10e1769b
김부연(윈컴) <ward6@naver.com>
2023-11-22 09:52:57 +0900

    1. Tool Window 작업중...

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Simple Json Compare
 
-Left URL과 Right URL의 Json 데이터를 서로 비교해서 다른 부분을 표시해 주는 intelliJ Plugin 입니다.
+Left URL과 Right URL의 Json 데이터를 서로 비교해서 다른 부분을 표시해 주는 IntelliJ Plugin 입니다.
 
 ### Java 11 or higher.
 
 ---
 
-### intelliJ version 2020.2.1 or later.
+### IntelliJ version 2020.2.1 or later.
 
 ---
 
@@ -14,17 +14,17 @@ Left URL과 Right URL의 Json 데이터를 서로 비교해서 다른 부분을 
 
 ---
 
-    1. intelliJ Menu > Tools > Simple Json Compare 선택
+    1. 오른쪽 Tool Window의 'Simple Json Compare' 클릭 또는 IntelliJ Menu > Tools > Simple Json Compare 선택
 
-    1. Method 선택
+    2. Method 선택
 
-    2. Left URL 입력 (http://ip:port/path?parameter)
+    3. Left URL 입력 (http://ip:port/path?parameter)
 
-    3. Right URL 입력 (http://ip:port 또는 http://ip:port/path?parameter)
+    4. Right URL 입력 (http://ip:port 또는 http://ip:port/path?parameter)
 
-    4. 필요한 경우 Body 입력
+    5. 필요한 경우 Body 입력
 
-    5. Compare 버튼 클릭
+    6. Compare 버튼 클릭
 
     * 아래 GIF 참조
 
--- src/main/java/kr/co/wincom/sjc/SjcToolWindowFactory.java
+++ src/main/java/kr/co/wincom/sjc/SjcToolWindowFactory.java
@@ -2,10 +2,12 @@ package kr.co.wincom.sjc;
 
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.jcef.JBCefApp;
 import kr.co.wincom.sjc.type.DialogToolWindowType;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,6 +15,11 @@ public class SjcToolWindowFactory implements ToolWindowFactory, DumbAware {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        if (!JBCefApp.isSupported()) {
+            Messages.showMessageDialog(project, "JCEF is not supported.", "", Messages.getErrorIcon());
+            return;
+        }
+
         CompareForm compareForm = new CompareForm(DialogToolWindowType.TOOL_WINDOW);
         compareForm.init();
         Content content = ContentFactory.SERVICE.getInstance().createContent(compareForm.getMainPanel(), "", false);
